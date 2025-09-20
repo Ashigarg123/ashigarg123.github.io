caf2593506331f7dc0a58d8e33efc1522dfe5f20
Ashigarg123 <54204052+Ashigarg123@users.noreply.github.com>
2025-09-20 19:34:18 +0000

    Deploying to gh-pages from @ Ashigarg123/ashigarg123.github.io@02f470b2c0764fa0883609fc01a86b7fae2e9816 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,9 +80,9 @@ ninja.data = [{
             window.open("https://www.geeksforgeeks.org/user/tanishkaashi567/contributions/", "_blank");
           
         },
-      },{id: "post-ash56sciencefreaks-my-hashnode-blog",
+      },{id: "post-my-hashnode-blog",
         
-          title: 'Ash56ScienceFreaks: My Hashnode Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: 'My Hashnode Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "A collection of my science and tech blog posts on Hashnode under the alias Ash56ScienceFreaks.",
         section: "Posts",
