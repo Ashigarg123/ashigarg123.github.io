68055e31b78ab780d9030cd2602f28912698c5bd
Ashigarg123 <54204052+Ashigarg123@users.noreply.github.com>
2025-09-20 18:55:12 +0000

    Deploying to gh-pages from @ Ashigarg123/ashigarg123.github.io@61900b685462eee95e04e400341bc09b1de311e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -410,28 +410,6 @@ ninja.data = [{
             window.location.href = "/sample-posts/2015/10/20/math.html";
           
         },
-      },{id: "post-a-post-with-code",
-        
-          title: "a post with code",
-        
-        description: "an example of a blog post with some code",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/sample-posts/2015/07/15/code.html";
-          
-        },
-      },{id: "post-a-post-with-images",
-        
-          title: "a post with images",
-        
-        description: "this is what included images could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/sample-posts/2015/05/15/images.html";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
