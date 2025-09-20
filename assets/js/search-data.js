2c1e1489da07cee9a29acc9fc620b2ac99f5dfb6
Ashigarg123 <54204052+Ashigarg123@users.noreply.github.com>
2025-09-20 18:35:17 +0000

    Deploying to gh-pages from @ Ashigarg123/ashigarg123.github.io@9d6a45661a4afe5e5126274f844d042b1dc9e044 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -344,17 +344,6 @@ ninja.data = [{
             window.location.href = "/assets/pdf/example_pdf.pdf";
           
         },
-      },{id: "post-ash56sciencefreaks-my-hashnode-blog",
-        
-          title: 'Ash56ScienceFreaks: My Hashnode Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "A collection of my science and tech blog posts on Hashnode under the alias Ash56ScienceFreaks.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://ash56sciencefreaks.hashnode.dev", "_blank");
-          
-        },
       },{id: "post-a-post-with-diagrams",
         
           title: "a post with diagrams",
