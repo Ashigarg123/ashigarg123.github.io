45390b01a1e0faa882e0e0d0e0f9ff67f558bae9
Ashigarg123 <54204052+Ashigarg123@users.noreply.github.com>
2025-09-20 19:49:07 +0000

    Deploying to gh-pages from @ Ashigarg123/ashigarg123.github.io@d30be6d025a963593f29b3d89031189e19b0eb5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,21 +23,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-build-a-passwordless-authentication-system-using-django-twilio-verify-and-sendgrid",
+        },{id: "nav-submenus",
+          title: "submenus",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/_pages/dropdown.html";
+          },
+        },{id: "post-build-a-passwordless-authentication-system-using-django-twilio-verify-and-sendgrid",
         
           title: 'Build a Passwordless Authentication System Using Django, Twilio Verify, and SendGrid <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
